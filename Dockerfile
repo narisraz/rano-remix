fd1b1f052ed602b05bd5c3e78fdcfe7de3a9f7ed
Naris Razafimahatratra <nrazafimahatratra.ext@softwaymedical.fr>
2022-07-18 13:33:12 +0300

    🗑️ clean and upgrade

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,14 @@
 # base node image
 FROM node:16-bullseye-slim as base
 
-# set for base and all layer that inherit from it
-ENV NODE_ENV production
-
 # Install openssl for Prisma
 RUN apt-get update && apt-get install -y openssl
 
 # Install all node_modules, including dev dependencies
 FROM base as deps
 
-WORKDIR /myapp
+RUN mkdir /app
+WORKDIR /app
 
 ADD package.json package-lock.json ./
 RUN npm install --production=false
@@ -18,36 +16,45 @@ RUN npm install --production=false
 # Setup production node_modules
 FROM base as production-deps
 
-WORKDIR /myapp
+RUN mkdir /app
+WORKDIR /app
 
-COPY --from=deps /myapp/node_modules /myapp/node_modules
+COPY --from=deps /app/node_modules /app/node_modules
 ADD package.json package-lock.json ./
 RUN npm prune --production
 
 # Build the app
 FROM base as build
 
-WORKDIR /myapp
+ENV NODE_ENV=production
 
-COPY --from=deps /myapp/node_modules /myapp/node_modules
+RUN mkdir /app
+WORKDIR /app
 
-ADD prisma .
-RUN npx prisma generate
+COPY --from=deps /app/node_modules /app/node_modules
+
+# If we're using Prisma, uncomment to cache the prisma schema
+# ADD prisma .
+# RUN npx prisma generate
 
 ADD . .
-RUN npm run postinstall
 RUN npm run build
 
 # Finally, build the production image with minimal footprint
 FROM base
 
-WORKDIR /myapp
+ENV NODE_ENV=production
 
-COPY --from=production-deps /myapp/node_modules /myapp/node_modules
-COPY --from=build /myapp/node_modules/.prisma /myapp/node_modules/.prisma
+RUN mkdir /app
+WORKDIR /app
 
-COPY --from=build /myapp/build /myapp/build
-COPY --from=build /myapp/public /myapp/public
+COPY --from=production-deps /app/node_modules /app/node_modules
+
+# Uncomment if using Prisma
+# COPY --from=build /app/node_modules/.prisma /app/node_modules/.prisma
+
+COPY --from=build /app/build /app/build
+COPY --from=build /app/public /app/public
 ADD . .
 
-CMD ["npm", "start"]
+CMD ["npm", "run", "start"]
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,11 +1,6 @@
-import { CacheProvider } from "@emotion/react";
-import createEmotionServer from '@emotion/server/create-instance';
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import type { EntryContext } from "@remix-run/node";
+import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
-import type { EntryContext } from "remix";
-import { RemixServer } from "remix";
-import createEmotionCache from "../src/createEmotionCache";
-import theme from "../src/theme";
 
 export default function handleRequest(
   request: Request,
@@ -13,37 +8,8 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const cache = createEmotionCache();
-  const { extractCriticalToChunks } = createEmotionServer(cache);
-
-  const MuiRemixServer = () => (
-    <CacheProvider value={cache}>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <RemixServer context={remixContext} url={request.url} />
-      </ThemeProvider>
-    </CacheProvider>
-  );
-
-  // Render the component to a string.
-  const html = renderToString(<MuiRemixServer />);
-
-  // Grab the CSS from emotion
-  const { styles } = extractCriticalToChunks(html);
-
-  let stylesHTML = '';
-
-  styles.forEach(({ key, ids, css }: any) => {
-    const emotionKey = `${key} ${ids.join(' ')}`;
-    const newStyleTag = `<style data-emotion="${emotionKey}">${css}</style>`;
-    stylesHTML = `${stylesHTML}${newStyleTag}`;
-  });
-
-  // Add the emotion style tags after the insertion point meta tag
-  const markup = html.replace(
-    /<meta(\s)*name="emotion-insertion-point"(\s)*content="emotion-insertion-point"(\s)*\/>/,
-    `<meta name="emotion-insertion-point" content="emotion-insertion-point"/>${stylesHTML}`,
+  const markup = renderToString(
+    <RemixServer context={remixContext} url={request.url} />
   );
 
   responseHeaders.set("Content-Type", "text/html");
